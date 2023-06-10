a643fa566c03c2e12fdcfdf81a8ef9ce1d0f7901
Muzammil-Ali-Khan <muzammilali1512@gmail.com>
2023-06-10 16:03:03 +0500

    fix: login api fix

--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -138,7 +138,7 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   let {
     email,
     password,
